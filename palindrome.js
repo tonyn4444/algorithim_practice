4546330998814184b5aed40c6b2439dd633659ac
Tony Nguyen <tntuyen@ucdavis.edu>
2017-05-06 12:10:27 -0700

    add regex for palindrome problems to remove special characeters and spacing

--- palindrome.js
+++ palindrome.js
@@ -4,14 +4,29 @@
 
 // 3. Compare original to reversed string and return true if equal
 
+// UPDATE: 
+
+// Remove special characters before split & reverse
+// Remove extra spaces before split & reverse
 
 function palindrome(str) {
-  var strArray = str.split('');
+  var removeSpecialChar = str.replace(/[^a-zA-Z ]/g, "");
+  removeSpecialChar = str.replace(/\s/g, '');
+
+  console.log(removeSpecialChar);
+  var strArray = removeSpecialChar.split('');
   var reversed = strArray.reverse().join('');
   
-  console.log(str, reversed);
+  // console.log(str, reversed);
   
-  return str == reversed;
+  return removeSpecialChar == reversed;
   
 }
+
+palindrome("_eye")
+palindrome("race car")
+
+
+
+
 
