f036d23737826f083aa2193dfc55dd8e854405a6
Tony Nguyen <tntuyen@ucdavis.edu>
2017-05-08 20:48:54 -0700

    update binary search tree superbalanced problem

---
+++ SuperBalanced.js
@@ -0,0 +1,3 @@
+// write a function that checks to see if a binary tree is 'superbalanced'
+// a 'superbalanced' tree is one where the difference in the depths between any two
+// leaf nodes is no greater than 1 
