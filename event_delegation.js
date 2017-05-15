73de0fb163dfb58e52e4b7ec9530259185b90e78
Tony Nguyen <tntuyen@ucdavis.edu>
2017-05-14 22:04:27 -0700

    add event_delegation problem

---
+++ event_delegation.js
@@ -0,0 +1,3 @@
+// Event Delegation allows us to add a single event listener to a 
+// parent element. This event listener will be activated on every existing child element,
+// as well elements that are added in the future.
